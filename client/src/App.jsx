3dab0d7b1a0eb94ac7e6bd2933e94add6a600015
non56345 <njad35699@gmail.com>
2025-12-16 22:45:47 +0300

    fix: move useEffect inside App and wake up server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
 import AnimatedRoutes from "./AnimatedRoutes";
+import api from "./api/axios";
 
 export default function App() {
+  useEffect(() => {
+    api.get("/").catch(() => {});
+  }, []);
+
   return <AnimatedRoutes />;
 }
-useEffect(() => {
-  api.get("/").catch(() => {});
-}, []);
-
